d2525440030e65c20bf53ddd40586c8644b2ac86
Avnen <avnen7@gmail.com>
2026-01-18 16:24:25 +0200

    added movement in script

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var plane: Sprite2D = $Plane
+@onready var helicopter: Sprite2D = $Carrier/Helicopter
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	plane.position.x += 50.0 * delta
+	helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
+	#print("helicopter.position: ", helicopter.scale)
+	#print("helicopter.global_scale: ", helicopter.global_scale)
 	pass
