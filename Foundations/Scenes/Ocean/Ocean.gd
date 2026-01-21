59cfde2c26bf807e1152f23b8781e6724868b798
Avnen <avnen7@gmail.com>
2026-01-21 09:54:08 +0200

    rotated the plane based on global position

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -6,12 +6,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	helicopter.global_rotation = plane.global_rotation
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.position.x += 50.0 * delta
-	helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
-	#print("helicopter.position: ", helicopter.scale)
-	#print("helicopter.global_scale: ", helicopter.global_scale)
+	plane.rotation_degrees += 30.0 * delta
+	#helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
+	#print("helicopter.position: ", rad_to_deg(helicopter.rotation) )
+	#print("helicopter.global_rotation: ", rad_to_deg(helicopter.global_rotation) )
 	pass
