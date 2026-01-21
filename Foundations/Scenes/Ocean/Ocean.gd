fe1adafabd4267b7f261a3cb52261dda07aefec7
Avnen <avnen7@gmail.com>
2026-01-21 14:58:13 +0200

    added movement of the plane using the arrow keys

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
 @onready var plane: Sprite2D = $Plane
-@onready var helicopter: Sprite2D = $Carrier/Helicopter
+@onready var helicopter: Sprite2D = $Helicopter
+
+#var _target: Vector2 = Vector2.ZERO
+const ROT_SPEED: float = PI / 2
+
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event.is_action_pressed("SetTarget"):
+#		_target = get_global_mouse_position()
+#		plane.look_at(_target)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	helicopter.global_rotation = plane.global_rotation
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.rotation_degrees += 30.0 * delta
-	#helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
-	#print("helicopter.position: ", rad_to_deg(helicopter.rotation) )
-	#print("helicopter.global_rotation: ", rad_to_deg(helicopter.global_rotation) )
+	if Input.is_action_pressed("ui_up"):
+		plane.move_local_x(50.0 * delta)
+	if Input.is_action_pressed("ui_right"):
+		plane.rotate(ROT_SPEED * delta)
+			
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(-ROT_SPEED * delta)
+	
+	#helicopter.position.x += 30.0 * delta
+	#helicopter.position.y += 50.0 * delta
+	#plane.position = plane.position.move_toward(_target, 50.0 * delta)
 	pass
